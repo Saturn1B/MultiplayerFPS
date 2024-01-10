010041c2fadfb73a6c3a74e30dac6e4983df74e9
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2024-01-10 11:21:16 +0100

    added fps cam, fix assembly definition for gameplay

--- Assets/FPS/Scripts/Network/Netcode/PlayerNetwork.cs
+++ Assets/FPS/Scripts/Network/Netcode/PlayerNetwork.cs
@@ -5,18 +5,45 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+
+	[SerializeField] private Camera cam;
+
+	Vector2 rotation = Vector2.zero;
+
+	float sensitivity = 150;
+	float yRotationLimit = 88;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
 	private void Update()
 	{
 		if (!IsOwner) return;
 
 		Vector3 moveDir = Vector3.zero;
 
-		if (Input.GetKey(KeyCode.W)) moveDir.z += 1;
-		if (Input.GetKey(KeyCode.S)) moveDir.z -= 1;
-		if (Input.GetKey(KeyCode.A)) moveDir.x -= 1;
-		if (Input.GetKey(KeyCode.D)) moveDir.x += 1;
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
 
-		float moveSpeed = 3;
+		moveDir = transform.right * x + transform.forward * z;
+
+		float moveSpeed = 5;
 		transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+		rotation.x += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+		rotation.y += -Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+		rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+		var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+		var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+
+		transform.localRotation = xQuat;
+		cam.transform.localRotation = yQuat;
+	}
+
+	public override void OnNetworkSpawn()
+	{
+		cam.enabled = IsLocalPlayer;
 	}
 }
