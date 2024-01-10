2996bc5e5a1b458dea1c1fc2b4ebf7b726dc7d2e
Romain Pitot <60229037+Globulous@users.noreply.github.com>
2024-01-10 15:53:43 +0100

    weapons

--- Assets/Pitot/Shotgun.cs
+++ Assets/Pitot/Shotgun.cs
@@ -1,28 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shotgun : MonoBehaviour
 {
-    public int bulletNumber = 8;
-    public float spreadAngle = 30f;
+    public int bulletNumber = 30; // Nombre de projectiles
+    public float spreadAngle = 7f; // Angle de dispersion
+    public int ralspread;
 
-    public Transform canonPos;
+    public Transform muzzlePoint; // Point de départ des projectiles
 
-    public void Update()
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Tir lors du clic gauche
+        {
+        FireShotgun();
+        }
+    }
+
+    void FireShotgun()
     {
         for (int i = 0; i < bulletNumber; i++)
         {
             // Générer une direction aléatoire dans un cône
-            Vector3 spreadDirection = Random.insideUnitCircle * spreadAngle;
-            Vector3 direction = transform.forward + new Vector3(spreadDirection.x, spreadDirection.y, 0f);
+            Vector3 spreadDirection = Random.insideUnitCircle.normalized * Random.Range(0f, spreadAngle);
+            Vector3 direction = new Vector3(Random.Range(-spreadAngle, spreadAngle) * Mathf.Cos(Random.Range(0, 2 * Mathf.PI)), Random.Range(-spreadAngle, spreadAngle) * Mathf.Sin(Random.Range(0, 2 * Mathf.PI)), ralspread);
 
             // Lancer un raycast dans la direction définie
-            if (Physics.Raycast(canonPos.position, direction, out RaycastHit hit, Mathf.Infinity))
+            RaycastHit hit;
+            if (Physics.Raycast(muzzlePoint.position, direction, out hit))
             {
-                // Si le raycast frappe un objet, effectuez les actions nécessaires (dégâts, effets, etc.)
                 Debug.Log("Hit object: " + hit.collider.gameObject.name);
             }
+            else
+            {
+                Debug.Log("Missed!");
+            }
+
+            // Affichage visuel du raycast
+            Debug.DrawRay(muzzlePoint.position, direction * 100f, Color.red, 3f);
         }
     }
 }
--- Assets/Pitot/Sniper.cs
+++ Assets/Pitot/Sniper.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Sniper : MonoBehaviour
 {
+
+    public float maxRaycastDistance = 100f;
+
     public void Update()
     {
         // Faire le tir du sniper avec un raycast orienté vers le haut (90 degrés)
         RaycastHit hit;
-        Debug.DrawRay(transform.position, transform.up, Color.red, 0.5f);
+        Debug.DrawRay(transform.position, transform.up * maxRaycastDistance, Color.red, 0.5f);
+        
         if (Input.GetMouseButtonDown(0)) // Vérifier si le bouton de la souris (clic gauche) est enfoncé
         {
-            if (Physics.Raycast(transform.position, transform.up, out hit))
+            if (Physics.Raycast(transform.position, transform.up, out hit, maxRaycastDistance))
             {
                 Debug.Log("Sniper shot hit: " + hit.collider.gameObject.name);
             }
