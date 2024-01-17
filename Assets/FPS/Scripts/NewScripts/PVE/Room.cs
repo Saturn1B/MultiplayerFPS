f7e22ca5ddad50bc5188f319c8f3cbe0f5fe168b
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2024-01-17 11:37:39 +0100

    start room detect player

--- Assets/FPS/Scripts/NewScripts/PVE/Room.cs
+++ Assets/FPS/Scripts/NewScripts/PVE/Room.cs
@@ -7,7 +7,7 @@ public enum RoomType
 {
 	STARTING,
 	NORMAL,
-	DOOR,
+	PORTAL,
 	BOSS
 }
 
@@ -15,4 +15,70 @@ public class Room : NetworkBehaviour
 {
 	[SerializeField] private RoomType roomType;
 	[SerializeField] private List<GameObject> roomEnemys;
+
+	private int playerOnRoom;
+	private GameManager gameManager;
+	private bool isDoorOpened;
+
+	public override void OnNetworkSpawn()
+	{
+		gameManager = FindObjectOfType<GameManager>();
+
+		foreach(GameObject enemy in roomEnemys)
+		{
+			enemy.GetComponent<HealthComponent>().OnDeath += OnEnemyDeathServerRpc;
+		}
+	}
+
+	[ServerRpc]
+	private void OnEnemyDeathServerRpc()
+	{
+		Debug.Log("Enemy died");
+		roomEnemys.RemoveAll(s => s == null);
+		if(roomEnemys.Count <= 0)
+		{
+			switch (roomType)
+			{
+				case RoomType.NORMAL:
+					OpenDoor();
+					break;
+				case RoomType.PORTAL:
+					ActivatePortal();
+					break;
+				case RoomType.BOSS:
+					//END GAME
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	private void OpenDoor()
+	{
+		Debug.Log("Open Door");
+		isDoorOpened = true;
+	}
+
+	private void ActivatePortal()
+	{
+		Debug.Log("Activate Portal");
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Trigger");
+		if (!IsHost) return;
+		Debug.Log("Trigger2");
+
+		if (other.transform.GetComponent<PlayerNetworkHandler>() && roomType == RoomType.STARTING && !isDoorOpened)
+		{
+			Debug.Log("Trigger3");
+			playerOnRoom++;
+			if(playerOnRoom >= gameManager.maxPlayers.Value)
+			{
+				OpenDoor();
+			}
+		}
+	}
 }
